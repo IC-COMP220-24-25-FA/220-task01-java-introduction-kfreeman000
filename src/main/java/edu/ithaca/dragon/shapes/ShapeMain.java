9aa372a1f8b4ac0e3336a65dfa132acdd078f7cb
Katherine Freeman <katherinemaryfreeman123@gmail.com>
2024-09-03 12:19:05 -0400

    printing objects

--- src/main/java/edu/ithaca/dragon/shapes/ShapeMain.java
+++ src/main/java/edu/ithaca/dragon/shapes/ShapeMain.java
@@ -1,4 +1,6 @@
 package edu.ithaca.dragon.shapes;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ShapeMain {
     
@@ -6,5 +8,16 @@ public class ShapeMain {
         //Make a list of 5 randomly-sized rectangles and print their area and the largest line that can be drawn through them
         //Allow the user to choose one, double the size of that one, and print them all again
         //Use a loop to repeat the process 5 times
+        List<Rectangle> aList = new ArrayList<Rectangle>();
+        aList.add(new Rectangle(10,4));
+        aList.add(new Rectangle(7,3));
+        aList.add(new Rectangle(1,3));
+        aList.add(new Rectangle(25,10));
+        aList.add(new Rectangle(5,8));
+        for (int i = 0; i < aList.size(); i++) {
+            System.out.println(aList.get(i));
+        }
+
+
     }
 }
