b2e39d6a64f4f81f5966b3afafb5556123043de0
Katherine Freeman <katherinemaryfreeman123@gmail.com>
2024-09-12 15:23:44 -0400

    rectangle class has all of these isues all of a sudden

--- src/main/java/edu/ithaca/dragon/shapes/ShapeMain.java
+++ src/main/java/edu/ithaca/dragon/shapes/ShapeMain.java
@@ -27,41 +27,34 @@ public class ShapeMain {
             shapeList.add(new Triangle(2, 4));
         }
        }   
-    System.out.println(shapeList);
+    System.out.println();
    
 
-        
-        
-        
-        
-        
-        
-        
-        
-        List<Rectangle> aList = new ArrayList<Rectangle>();
-        aList.add(new Rectangle(10,4));
-        aList.add(new Rectangle(7,3));
-        aList.add(new Rectangle(1,3));
-        aList.add(new Rectangle(25,10));
-        aList.add(new Rectangle(5,8));
-        Scanner scanner = new Scanner(System.in);
-        for (int i = 0; i < aList.size(); i++) {
-            System.out.println(aList.get(i).calcArea());
-            System.out.println(aList.get(i).longestLineWithin());
-        for (int x = 0; x < aList.size(); x++) {
-            System.out.println("choose a rectangle index 0-4");
-            int index = Integer.parseInt(scanner.nextLine());
-            aList.get(index).doubleSize();
-            for (int i2 = 0; i2 < aList.size(); i2++) {
-                System.out.println(aList.get(i2).calcArea());
-                System.out.println(aList.get(i2).longestLineWithin());
-        }
-        
-        }
-        
-        scanner.close();
+    List<Rectangle> aList = new ArrayList<Rectangle>();
+    aList.add(new Rectangle(10,4));
+    aList.add(new Rectangle(7,3));
+    aList.add(new Rectangle(1,3));
+    aList.add(new Rectangle(25,10));
+    aList.add(new Rectangle(5,8));
+    Scanner scanner = new Scanner(System.in);
+    for (int i = 0; i < aList.size(); i++) {
+        System.out.println(aList.get(i).calcArea());
+        System.out.println(aList.get(i).longestLineWithin());
+    for (int x = 0; x < aList.size(); x++) {
+        System.out.println("choose a rectangle index 0-4");
+        int index = Integer.parseInt(scanner.nextLine());
+        aList.get(index).doubleSize();
+        for (int i2 = 0; i2 < aList.size(); i2++) {
+            System.out.println(aList.get(i2).calcArea());
+            System.out.println(aList.get(i2).longestLineWithin());
+    }
+    
+    }
+    
+    scanner.close();
 
-        }
+    }    
+        
 
     }
 }
